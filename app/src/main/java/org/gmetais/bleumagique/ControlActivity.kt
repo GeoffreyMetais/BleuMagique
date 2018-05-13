05276a042f3c2d2e7a4e21d4ca1f6e2edb9da491
Geoffrey Métais <geoffrey.metais@gmail.com>
2018-05-13 14:16:32 +0200

    Light bulb on UI

--- app/src/main/java/org/gmetais/bleumagique/ControlActivity.kt
+++ app/src/main/java/org/gmetais/bleumagique/ControlActivity.kt
@@ -7,7 +7,6 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.SeekBar
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_control.*
 
 private const val TAG = "BLE/ControlActivity"
