a24e483905aba949c7f0e675d82dfd44821b6be5
Geoffrey Métais <geoffrey.metais@gmail.com>
2018-05-14 23:06:20 +0200

    Device description command

--- app/src/main/java/org/gmetais/bleumagique/BlueController.kt
+++ app/src/main/java/org/gmetais/bleumagique/BlueController.kt
@@ -3,26 +3,32 @@ package org.gmetais.bleumagique
 import android.arch.lifecycle.MutableLiveData
 import android.bluetooth.*
 import android.content.Context
+import android.util.Log
 import java.util.*
 
 private const val TAG = "BLE/BlueController"
+private const val MB_CONTROL_SERVICE = "0000ffe5-0000-1000-8000-00805f9b34fb"
+private const val MB_CONTROL_CHAR = "0000ffe9-0000-1000-8000-00805f9b34fb"
 private const val MB_OFF = "CC2433"
 private const val MB_ON = "CC2333"
-private const val MB_SERVICE = "0000ffe5-0000-1000-8000-00805f9b34fb"
+private const val MB_DESCRIPTION_REQ = "EF0177"
 
-private const val MB_CONTROL = "0000ffe9-0000-1000-8000-00805f9b34fb"
-private const val MB_NOTIFY = "0000ffe4-0000-1000-8000-00805f9b34fb"
-private const val CONFIG = "00002902-0000-1000-8000-00805f9b34fb"
+private const val MB_NOTIFY_SERVICE = "0000ffe0-0000-1000-8000-00805f9b34fb"
+private const val MB_NOTIFY_CHAR = "0000ffe4-0000-1000-8000-00805f9b34fb"
+private const val MB_DESCRIPTOR = "00002902-0000-1000-8000-00805f9b34fb"
 
 class BlueController(appCtx: Context, private val isOn: MutableLiveData<Boolean>, private val connected: MutableLiveData<Boolean>) {
 
     private var btGatt : BluetoothGatt? = null
     private var btAdapter : BluetoothAdapter? = null
-    private var service : BluetoothGattService? = null
-    private var characteristic : BluetoothGattCharacteristic? = null
+    private var ctrlService : BluetoothGattService? = null
+    private var notifyService : BluetoothGattService? = null
+    private var notifyCharacteristic : BluetoothGattCharacteristic? = null
+    private var ctrlCharacteristic : BluetoothGattCharacteristic? = null
 
-    private fun send(command: String) = characteristic?.let {
-        btGatt?.writeCharacteristic(it.apply { value = hexStringToByteArray(command) })
+    private fun send(command: String) = ctrlCharacteristic?.let {
+        it.writeType = BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
+        Log.d(TAG, "send $command:  ${btGatt?.writeCharacteristic(it.apply { value = hexStringToByteArray(command) })}")
     }
 
     fun toggle() = send(if (isOn.value == true) MB_OFF else MB_ON)
@@ -34,13 +40,22 @@ class BlueController(appCtx: Context, private val isOn: MutableLiveData<Boolean>
             else connected.postValue(false)
         }
 
-        override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
-            service = gatt?.getService(UUID.fromString(MB_SERVICE))
-            characteristic = service?.getCharacteristic(UUID.fromString(MB_CONTROL))
-            characteristic?.let {
-                gatt?.setCharacteristicNotification(it, true)
+        override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+            ctrlService = gatt.getService(UUID.fromString(MB_CONTROL_SERVICE))
+            ctrlCharacteristic = ctrlService?.getCharacteristic(UUID.fromString(MB_CONTROL_CHAR))
+            notifyService = gatt.getService(UUID.fromString(MB_NOTIFY_SERVICE))
+            notifyCharacteristic = notifyService?.getCharacteristic(UUID.fromString(MB_NOTIFY_CHAR))
+            notifyCharacteristic?.let {
+                gatt.setCharacteristicNotification(it, true)
+                val descriptor = it.getDescriptor(UUID.fromString(MB_DESCRIPTOR))
+                descriptor.value = BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE
+                gatt.writeDescriptor(descriptor)
                 connected.postValue(true)
             }
+        }
+
+        override fun onDescriptorWrite(gatt: BluetoothGatt, descriptor: BluetoothGattDescriptor, status: Int) {
+            send(MB_DESCRIPTION_REQ)
         }
 
         override fun onCharacteristicWrite(gatt: BluetoothGatt?, characteristic: BluetoothGattCharacteristic?, status: Int) {
@@ -52,6 +67,11 @@ class BlueController(appCtx: Context, private val isOn: MutableLiveData<Boolean>
             }
             gatt!!.readCharacteristic(characteristic!!)
         }
+
+        override fun onCharacteristicChanged(gatt: BluetoothGatt?, characteristic: BluetoothGattCharacteristic?) {
+            Log.d(TAG, "onCharacteristicChanged ${characteristic!!.uuid}, ${characteristic.value.toHexString()}")
+        }
+
     }
 
     init {
